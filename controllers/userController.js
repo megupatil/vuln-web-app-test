896078dcbec46bbd1a4d31ec59ee7acf7c1aef56
Meghraj <megupatil@gmail.com>
2026-05-05 14:37:33 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,16 @@
+const userService = require('../services/userService');
+const upload = require('../utils/fileUpload');
 
+exports.getUser = (req, res) => {
+    const name = req.query.name;
+    const result = userService.getUser(name);
+    res.send(result);
+};
+
+// No validation ❌
+exports.uploadFile = [
+    upload.single('file'),
+    (req, res) => {
+        res.send("File uploaded");
+    }
+];
