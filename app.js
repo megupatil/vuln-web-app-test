0b21bb20c40c5441afa1eb3a0a4f10930024203b
Meghraj <megupatil@gmail.com>
2026-05-05 14:36:08 +0530

    Set up Express server with user routes

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 
+const app = express();
+app.use(express.json());
+
+// No security headers (intentional weakness)
+app.use('/api', userRoutes);
+
+app.listen(3000, () => {
+    console.log('App running on port 3000');
+});
